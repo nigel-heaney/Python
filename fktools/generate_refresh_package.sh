4a5c744058f7cb0cfb37d2d3a26f66c77c9fddcb
nigel.heaney@gmail.com <nigel.heaney@gmail.com>
2017-05-29 17:09:04 +0100

    fktools and related

---
+++ fktools/generate_refresh_package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+############################################################################
+# Filename:     generate_refresh_package
+# Written by:   Nigel Heaney
+# Decription:   simple script to generate a refresh list and packages
+#
+# Version:      1.0
+# Version       Date            Author          Description
+# 1.0           03/01/2013      Nigel Heaney    initial version
+############################################################################
+python $FK_HOME/bin/wssfktools.py
+
+
